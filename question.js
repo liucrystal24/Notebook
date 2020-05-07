a7fa53c77b3e227c7013b0887009d95679b542bb
liucrystal24 <liucrystal_24@outlook.com>
2020-05-08 00:05:08 +0800

    question.js & test2.js

--- question.js
+++ question.js
@@ -19,7 +19,7 @@ var a = 12;
 
 var bar1 = function () {
   foo1.call(obj1);
-};
+}; // 此时尚未执行 ？区别
 bar1(); // 1 this -> obj1
 bar1.call(obj2) //1  this -> obj1
 // ??硬绑定 锁死?? 先执行 foo1.call(obj1)
@@ -27,6 +27,7 @@ bar1.call(obj2) //1  this -> obj1
 // call定义看一下
 
 var bar2 = foo2.call(obj1); // call(?) ?是什么都一样，不影响结果, this消失??
+// 此时已经执行 ？区别
 // bar2 是 function
 bar2(); // 12
 bar2.call(obj2) // 2
